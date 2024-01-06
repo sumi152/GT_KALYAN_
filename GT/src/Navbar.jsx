7bd75d238ab00ef730f31155185623b30136965f
sumit <sumitrohit2004@gmail.com>
2024-01-06 20:09:09 +0530

    navbar responsive

--- GT/src/Navbar.jsx
+++ GT/src/Navbar.jsx
@@ -15,9 +15,9 @@ function Navbar() {
     
     return (
 
-        <nav className="bg-custom-purple text-white p-4">
-            <div className="container mx-auto flex justify-between items-center">
-                <div className="flex items-center">
+        <nav className="bg-custom-purple text-white relative p-4">
+            <div className="container mx-auto flex  flex-1 justify-between items-center">
+                <div className="flex items-center min-w-0">
                     <button onClick={handleSidebarToggle} className="mr-4">
                         <img src={HamburgerIcon} alt="Hamburger Icon" className="w-8 h-8" />
                     </button>
@@ -26,7 +26,7 @@ function Navbar() {
                         </marquee>
                     </div>
                 </div>
-                <ul className="font-bold text-lg flex space-x-4">
+                <ul className="font-bold text-lg flex flex-shrink: 0 absolute right-10 top-15">
                     <li className="flex items-center">
                         <a href="#" className="text-white flex items-center">
                             <img src={WalletIcon} alt="Wallet Icon" className="w-8 h-8 mr-2" />
