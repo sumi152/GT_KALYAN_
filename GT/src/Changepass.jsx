8ebc7c2f55d04b1af97932362bf6e678faaa3796
sumit <sumitrohit2004@gmail.com>
2024-03-04 11:43:44 +0530

    Change Password Completed

--- GT/src/Changepass.jsx
+++ GT/src/Changepass.jsx
@@ -1,11 +1,11 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import topBackground from "./Images/bg.png";
 import changePassword from "./Images/change_password_icon.png";
 import { FiEye, FiEyeOff, FiLock } from "react-icons/fi";
 import { BiArrowBack } from "react-icons/bi";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-
+import { addPass } from "./Util/passslice";
 function Changepass() {
   const navbarStyle = {
     height: "60px",
@@ -17,7 +17,7 @@ function Changepass() {
     backgroundImage: `url(${topBackground})`,
     backgroundSize: "cover",
   };
-
+  const dispatch = useDispatch();
   const currentPassword = useRef();
   const newPassword = useRef();
   const confirmPassword = useRef();
@@ -27,8 +27,8 @@ function Changepass() {
   const [formErrors, setFormErrors] = useState({});
   const [errorText, setErrorText] = useState("");
   const [msg, setmsg]= useState("");
+  const [issubmit,setIsSubmit]= useState(false);
 
-  
   const handleToggleCurrentPassword = () => {
     setShowCurrentPassword(!showCurrentPassword);
   };
@@ -40,12 +40,28 @@ function Changepass() {
   const handleToggleConfirmPassword = () => {
     setShowConfirmPassword(!showConfirmPassword);
   };
+
   const navigate = useNavigate();
   const back = () => {
     navigate("/imp");
   };
+
+  const handleAdduser = (password) => {
+    dispatch(addPass({password: password}));
+  };
   
   const token = useSelector(state => state.userDetail.token);
+  const pass= useSelector(state=> state.userDetail2.password);
+  console.log(pass);
+
+  useEffect(() => {
+    if (issubmit && msg) {
+      // Password changed successfully
+    }
+    if (issubmit && errorText) {
+      // Handle error
+    }
+  }, [issubmit, msg, errorText]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -62,10 +78,12 @@ function Changepass() {
     }
 
     try {
-       await fetchData(currentPassword.current.value, newPassword.current.value,token);
+      await fetchData(currentPassword.current.value, newPassword.current.value,token);
       setIsSubmit(true);
+      setmsg('Successfully changed password');
+      handleAdduser(newPassword.current.value);
     } catch (error) {
-      setErrorText("Old password is incorrect"); // Set error message
+      setErrorText("Old password is incorrect");
     }
   };
 
@@ -75,6 +93,9 @@ function Changepass() {
     if (!currentPassword) {
       errors.conf0 = "Current Password is required!";
     }
+    else if(currentPassword !== pass){
+      errors.conf4 = 'Old password does not match';
+    }
 
     if (!newPassword) {
       errors.conf1 = "New Password is required!";
@@ -82,13 +103,13 @@ function Changepass() {
     if (!confirmPassword) {
       errors.conf2 = "Confirm Password is required!";
     }
-    if (newPassword && confirmPassword &&newPassword != confirmPassword) {
+    if (newPassword && confirmPassword && newPassword !== confirmPassword) {
       errors.conf3 = "Confirm Password is not equal to New Password!";
     }
     return errors;
   };
 
-  const fetchData = async ( currentPassword, newPassword,token) => {
+  const fetchData = async (currentPassword, newPassword, token) => {
     const myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
     myHeaders.append(
@@ -97,12 +118,11 @@ function Changepass() {
     );
 
     const raw = JSON.stringify({
-      
-        env_type: "Prod",
-        app_key: "jAFaRUulipsumXLLSLPFytYvUUsgfh",
-        unique_token: token,
-        old_pass: currentPassword,
-        new_pass: newPassword
+      env_type: "Prod",
+      app_key: "jAFaRUulipsumXLLSLPFytYvUUsgfh",
+      unique_token: token,
+      old_pass: currentPassword,
+      new_pass: newPassword
     });
 
     const requestOptions = {
@@ -118,9 +138,8 @@ function Changepass() {
     );
     const result = await response.json();
 
-    if(result?.status===true){
-       setmsg(result?.msg);
-       console.log(msg+'updated sumti');
+    if(result?.status === true){
+      // Password changed successfully
     } else {
       throw new Error("Invalid username and password");
     }
@@ -138,7 +157,7 @@ function Changepass() {
       </div>
       <div style={changePassStyle}>
         <div className="flex justify-center items-center h-screen">
-        <form  onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit}>
           <div className="bg-transparent p-6 rounded-md shadow-md">
             <div className="mb-6 relative">
               <label className="block mb-1 font-medium text-white">
@@ -153,7 +172,7 @@ function Changepass() {
                   placeholder="Password"
                   ref={currentPassword}
                   className="w-full rounded-md border-gray-300 bg-gray-300 focus:ring-indigo-500 focus:border-indigo-500 pl-10 p-3 text-black"
-                  style={{ width: "300px" }} // Set the width here as per your requirement
+                  style={{ width: "300px" }}
                   name="password"
                 />
 
@@ -180,7 +199,7 @@ function Changepass() {
                   placeholder="Enter New Password"
                   ref={newPassword}
                   className="w-full rounded-md border-gray-300 bg-gray-300 focus:ring-indigo-500 focus:border-indigo-500 pl-10 p-3 text-black"
-                  style={{ width: "300px" }} // Set the width here as per your requirement
+                  style={{ width: "300px" }}
                   name="newPassword"
                 />
                 <button
@@ -206,7 +225,7 @@ function Changepass() {
                   placeholder="Enter Confirm Password"
                   ref={confirmPassword}
                   className="w-full rounded-md border-gray-300 bg-gray-300 focus:ring-indigo-500 focus:border-indigo-500 pl-10 p-3 text-black"
-                  style={{ width: "300px" }} // Set the width here as per your requirement
+                  style={{ width: "300px" }}
                   name="confirmPassword"
                 />
                 <button
@@ -218,8 +237,8 @@ function Changepass() {
                 </button>
                 <p className="text-red-500">{formErrors.conf2}</p>
                 <p className="text-red-500">{formErrors.conf3}</p>
+                <p className="text-red-500">{formErrors.conf4}</p>
                 <p className="text-green-500">{msg}</p>
-                
               </div>
             </div>
             <div className="flex justify-center items-center">
--- GT/src/Util/appStore.jsx
+++ GT/src/Util/appStore.jsx
@@ -1,8 +1,11 @@
-import {configureStore} from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit';
 import loginReducer from './loginSlice';
+import passReducer from './passslice'; // Import passReducer from passSlice.jsx
+
 const appStore = configureStore({
     reducer:{
         userDetail: loginReducer,
+        userDetail2: passReducer,
     }
 });
 
