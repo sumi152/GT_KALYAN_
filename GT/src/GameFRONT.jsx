aa50545766ce7fd445c712067a4ab340711ce5c6
sumi152 <skumar05012004@gmail.com>
2024-06-26 20:37:55 +0530

    Game result updated

--- GT/src/GameFRONT.jsx
+++ GT/src/GameFRONT.jsx
@@ -73,10 +73,13 @@ function GameFRONT() {
                 </a>
               </div>
               <p className="text-blue-900 font-bold">
-                {game.open_result && game.close_result
-                  ? `${game.open_result}${game.close_result}`
-                  : "***_**_***"}
-              </p>
+  {game.open_result
+    ? game.close_result
+      ? `${game.open_result}${game.close_result}`
+      : `${game.open_result}****`
+    : "***_**_***"}
+</p>
+
               <button
                 onClick={() => {
                   if (game.msg_status === 1) {
